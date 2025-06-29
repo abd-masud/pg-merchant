27bc254f59c79a40cb142c1687626ff8602759e0
abdMasud <abdmasud.2000@gmail.com>
2025-06-29 13:09:21 +0600

    merchant all pages

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -14,7 +14,7 @@ const HomePage = () => {
         </div>
 
         <Link href="/auth/login" className="button first">
-          <button>Login</button>
+          <button>Merchant Login</button>
           <span className="button-effect"></span>
         </Link>
 
